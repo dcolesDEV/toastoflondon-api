727e3499b18c7ebc2f563c43a01b0d685ca0396d
Daniel Coles <me@danielcoles.co.uk>
2021-10-09 01:16:43 +0100

    🚧 updated checks workflow to use nightly

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,55 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
 
 use std::fs::File;
 use std::io::Read;
 
-use rand::thread_rng;
 use rand::seq::SliceRandom;
+use rand::thread_rng;
 
-fn fetch_names() -> Result<Vec<String>, String> {
-    let mut file = File::open("names.json").unwrap();
-    let mut data = String::new();
+fn fetch_names(file_name: String) -> Result<Vec<String>, String> {
+    let file = File::open(file_name);
 
-    file.read_to_string(&mut data).unwrap();
+    match file {
+        Ok(mut opened_file) => {
+            let mut data = String::new();
 
-    let json = serde_json::from_str(&data);
-
-    match json {
-        Ok(json) => Ok(json),
-        Err(_) => Err(String::from("Error processing names"))
+            opened_file.read_to_string(&mut data).unwrap();
+            serde_json::from_str(&data).unwrap()
+        }
+        Err(_) => Err(String::from("File provided does not exist")),
     }
 }
 
 fn choose_random_name(names: Vec<String>) -> Result<String, String> {
     let mut rng = thread_rng();
-
-    if names.is_empty() {
-        return Err(String::from("No names available to be randomly selected from"))
-    }
-
     let random_name = names.choose(&mut rng);
 
     match random_name {
         Some(random_name) => Ok(String::from(random_name)),
-        None => Err(String::from("Random name was unable to be selected"))
+        None => Err(String::from(
+            "No names available to be randomly selected from",
+        )),
     }
 }
 
 #[get("/")]
 fn get() -> Result<String, String> {
-    let names = fetch_names();
+    let names = fetch_names(String::from("names.json"));
 
     match names {
         Ok(names) => {
             let result = choose_random_name(names);
-        
+
             match result {
                 Ok(name) => Ok(name),
-                Err(error) => Err(error)
+                Err(error) => Err(error),
             }
-        },
-        Err(error) => Err(error)
+        }
+        Err(error) => Err(error),
     }
-
 }
 
 fn rocket() -> rocket::Rocket {
@@ -66,46 +63,36 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use rocket::local::Client;
     use rocket::http::Status;
+    use rocket::local::Client;
 
     #[test]
     fn test_fetch_names() {
-        let names: Result<Vec<String>, String> = fetch_names();
+        let names = fetch_names(String::from("names.json")).unwrap();
 
-        match names {
-            Ok(names) => assert_eq!(names[4], "Kikini Bamalam"),
-            Err(_) => assert!(false)
-        }
+        assert_eq!(names[4], "Kikini Bamalam")
+    }
+
+    #[test]
+    #[should_panic(expected = "File provided does not exist")]
+    fn test_fetch_names_with_nonexistent_file() {
+        fetch_names(String::from("nonexistent.file")).unwrap();
     }
 
     #[test]
     fn test_choose_random_name() {
-        let names: Result<Vec<String>, String> = fetch_names();
+        let names = fetch_names(String::from("names.json")).unwrap();
+        let result = choose_random_name(names.to_owned()).unwrap();
 
-        match names {
-            Ok(names) => {
-                let result: Result<String, String> = choose_random_name(names.to_owned());
-                
-                match result {
-                    Ok(result) => assert_eq!(names.contains(&result), true),
-                    Err(_) => assert!(false)
-                }
-            },
-            Err(_) => assert!(false)
-        }
+        assert_eq!(names.contains(&result), true)
     }
 
     #[test]
+    #[should_panic(expected = "No names available to be randomly selected from")]
     fn test_choose_random_name_with_no_names() {
         let names: Vec<String> = vec![];
 
-        let result: Result<String, String> = choose_random_name(names.to_owned());
-        
-        match result {
-            Ok(_) => assert!(false),
-            Err(error) => assert_eq!(error, "No names available to be randomly selected from")
-        }
+        choose_random_name(names).unwrap();
     }
 
     #[test]
@@ -113,20 +100,10 @@ mod tests {
         let client = Client::new(rocket()).expect("valid rocket instance");
         let mut response = client.get("/").dispatch();
 
-        let names: Result<Vec<String>, String> = fetch_names();
+        let names = fetch_names(String::from("names.json")).unwrap();
+        let result = response.body_string().unwrap();
 
-        match names {
-            Ok(names) => {
-                let result: Option<String> = response.body_string();
-        
-                assert_eq!(response.status(), Status::Ok);
-                
-                match result {
-                    Some(result) => assert_eq!(names.contains(&result), true),
-                    None => assert!(false)
-                }
-            },
-            Err(_) => assert!(false)
-        }
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(names.contains(&result), true)
     }
 }
